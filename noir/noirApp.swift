ad477e00dbdfdb3fec28656d253eefec9d2d5128
pyro <me@pyrorhythm.dev>
2026-04-18 21:53:36 +0300

    Initial Commit

---
+++ noir/noirApp.swift
@@ -0,0 +1,17 @@
+//
+//  noirApp.swift
+//  noir
+//
+//  Created by Алексей Печенин on 18.4.2026.
+//
+
+import SwiftUI
+
+@main
+struct noirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ noirTests/noirTests.swift
@@ -0,0 +1,19 @@
+//
+//  noirTests.swift
+//  noirTests
+//
+//  Created by Алексей Печенин on 18.4.2026.
+//
+
+import Testing
+@testable import noir
+
+struct noirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
